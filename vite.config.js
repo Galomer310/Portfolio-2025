85fd278d0f9da79acc4de98cf590f30402fe70d9
Gal Omer <galomer6708@gmail.com>
2025-02-16 17:34:10 +0200

    Update base path in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  base: "/Portfolio-2025/",
+  base: "/", // Change this to match the deployment path
   plugins: [react()],
 });
